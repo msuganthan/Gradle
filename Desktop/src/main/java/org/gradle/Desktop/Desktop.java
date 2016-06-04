09048224550ce414f0cacd1a5b4a33d9463119ac
msuganthan <suganthanchandrika@gmail.com>
2016-06-04 14:22:40 +0530

    gradle

---
+++ Desktop/src/main/java/org/gradle/Desktop/Desktop.java
@@ -0,0 +1,13 @@
+package org.gradle.Desktop;
+
+public class Desktop {
+	private String desktopName;
+	
+	public String getDesktopName() {
+		return this.desktopName;
+	}
+	
+	public void setDesktopName(String desktopName) {
+		this.desktopName = desktopName;
+	}
+}
